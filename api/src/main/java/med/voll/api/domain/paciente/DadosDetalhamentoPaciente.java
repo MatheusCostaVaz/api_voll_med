a66614177ccad0d2f5f40f760796b3349e5dbf6c
Matheus Vaz <matheuscostav@outlook.com>
2023-10-13 12:27:25 -0300

    Testes automatizados v2

--- api/src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
+++ api/src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -2,9 +2,9 @@ package med.voll.api.domain.paciente;
 
 import med.voll.api.domain.endereco.Endereco;
 
-public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+public record DadosDetalhamentoPaciente(Long id, Boolean ativo,String nome, String email, String cpf, String telefone, Endereco endereco) {
 	
 	public DadosDetalhamentoPaciente(Paciente paciente) {
-		this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+		this(paciente.getId(), paciente.getAtivo(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
 	}
 }
